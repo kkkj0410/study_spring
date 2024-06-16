9a1328af58e814088f5d9b5f793aaeb340d53fbb
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-06-17 08:30:23 +0900

    OrderServiceImpl 테스트

---
+++ 스프링 핵심 원리-기본편/섹션3/강의3 : 관심사 분리/OrderApp.java
@@ -0,0 +1,27 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+
+public class OrderApp {
+    public static void main(String[] args)
+    {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+
+    }
+
+}
