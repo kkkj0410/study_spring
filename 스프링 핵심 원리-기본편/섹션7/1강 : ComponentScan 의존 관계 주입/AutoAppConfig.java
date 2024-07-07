53801695fa81613146d548f6971fa0eab3b96aea
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-07-08 08:58:43 +0900

    DI 컨테이너 ComponentScan 등록

---
+++ 스프링 핵심 원리-기본편/섹션7/1강 : ComponentScan 의존 관계 주입/AutoAppConfig.java
@@ -0,0 +1,11 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan
+public class AutoAppConfig {
+
+}
