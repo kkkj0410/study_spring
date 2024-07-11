e63a6f68b53d6e5cb81d3e2544e639ef8c7b1771
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-07-12 08:42:52 +0900

    중복된 스프링 빈 조회 방법
    1. 의존성 주입에 사용되는 변수명을 스프링 빈 객체이름과 동일하게 적용

---
+++ 스프링 핵심 원리-기본편/섹션7/5강 : 중복 스프링 빈/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// @Component
+// public class FixDiscountPolicy implements DiscountPolicy {
+// }
+
+// @Component
+// public class RateDiscountPolicy implements DiscountPolicy{
+// }
+
+@Component
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
+}
