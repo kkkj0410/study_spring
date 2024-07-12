61dfdf0182462f31a42c7428b8a848edcdc86b39
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-07-13 08:14:58 +0900

    어노테이션 적용 객체 활용

---
+++ 스프링 핵심 원리-기본편/섹션7/6강 : 어노테이션 생성/OrderServiceImpl.java
@@ -0,0 +1,13 @@
+@Component
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+}
