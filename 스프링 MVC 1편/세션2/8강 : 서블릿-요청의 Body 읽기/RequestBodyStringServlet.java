47bfd12511d2e4772f57c892552bc514745bd452
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-07-20 08:15:04 +0900

    서블릿-요청의 Body 읽기

---
+++ 스프링 MVC 1편/세션2/8강 : 서블릿-요청의 Body 읽기/RequestBodyStringServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        //요청받은 body의 데이터를 읽어들임
+        ServletInputStream inputStream = request.getInputStream();
+        
+        //받은 데이터는 바이트 형태로 저장됨. 따라서 문자열 형태로 재구성
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+
+}
