d556c6462a6b568e3e339446e76a81735d9afd20
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-07-28 08:14:28 +0900

    MemberFormControllerV1(개별 Controller)

---
+++ 스프링 MVC 1편/세션4/2강 : V1- 프론트 컨트롤러/MemberFormControllerV1.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+
+        //URL 지정
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+
+        //지정된 URL로 요청 보냄
+        dispatcher.forward(request, response);
+    }
+}
