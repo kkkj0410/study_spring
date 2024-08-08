ba0358eb28c740d64e792843a77421b3a9a7eeea
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-09 08:13:00 +0900

    테스트

---
+++ 스프링 DB 1편/세션1/6강 : JDBC 방식의 DB 데이터 삽입/MemberRepositoryV0Test.java
@@ -0,0 +1,21 @@
+package hello_DB.jdbc.repository;
+
+import hello_DB.jdbc.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException
+    {
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+    }
+
+}
