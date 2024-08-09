38437e283f981aa92e3301a6beea9b8420759ca5
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-10 08:07:29 +0900

    Jdbc - 특정 member_id의  DB의 데이터 값 호출

---
+++ 스프링 DB 1편/세션1/7강 : DB 데이터 조회/MemberRepositoryV0Test.java
@@ -0,0 +1,32 @@
+package hello_DB.jdbc.repository;
+
+import hello_DB.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException
+    {
+        //save
+        Member member = new Member("memberV8", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
