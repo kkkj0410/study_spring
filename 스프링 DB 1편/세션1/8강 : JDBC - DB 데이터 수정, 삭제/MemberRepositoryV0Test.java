4c0acfa158757fc6fe10d138c698893bd82e2eee
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-11 08:10:17 +0900

    테스트

---
+++ 스프링 DB 1편/세션1/8강 : JDBC - DB 데이터 수정, 삭제/MemberRepositoryV0Test.java
@@ -0,0 +1,45 @@
+package hello_DB.jdbc.repository;
+
+import hello_DB.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException
+    {
+        //save
+        Member member = new Member("memberV14", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        //update : money : 10000->20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+				//삭제된 데이터는 불러올 수 없음. 따라서 데이터 호출이 안될때 터지는
+				//NoSuchElementException예외를 매칭해서 검증함.
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+}
