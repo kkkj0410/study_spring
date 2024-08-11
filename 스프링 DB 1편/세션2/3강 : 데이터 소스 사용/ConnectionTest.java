a905b708adaa351db249d13999331a429bc00ab0
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-12 08:13:37 +0900

    드라이버 매니저 사용

---
+++ 스프링 DB 1편/세션2/3강 : 데이터 소스 사용/ConnectionTest.java
@@ -0,0 +1,27 @@
+package hello_DB.jdbc.connection;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello_DB.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+}
