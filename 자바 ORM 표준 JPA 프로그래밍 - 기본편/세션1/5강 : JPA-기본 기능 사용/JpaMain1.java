cedaffddd76f6478b7fda8044b37a616061ecf25
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-17 08:08:09 +0900

    데이터 추가 기능

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션1/5강 : JPA-기본 기능 사용/JpaMain1.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("helloA");
+
+            //h2에 저장
+            em.persist(member);
+
+            //h2에 저장한 거 등록
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+}
