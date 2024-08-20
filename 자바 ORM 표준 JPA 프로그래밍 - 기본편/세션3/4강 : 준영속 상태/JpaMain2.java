2a23d37597effc7e0e16bd64d747494e41d0e1ab
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-21 08:47:11 +0900

    2. em.clear()

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션3/4강 : 준영속 상태/JpaMain2.java
@@ -0,0 +1,31 @@
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member(125L, "member125");
+            member.setName("AAAA");
+
+            em.clear();
+
+            System.out.println("==================================");
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+}
