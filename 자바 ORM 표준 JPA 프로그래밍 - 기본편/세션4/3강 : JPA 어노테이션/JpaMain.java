8880f5874dd3d297115dc31dc8a8ab0ff8478c8b
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-24 08:57:27 +0900

    @Enumerated 예시

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션4/3강 : JPA 어노테이션/JpaMain.java
@@ -0,0 +1,31 @@
+
+import static hellojpa.RoleType.USER;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setRoleType(USER);
+            member.setId(123L);
+            member.setUsername("asd");
+            em.persist(member);
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+}
