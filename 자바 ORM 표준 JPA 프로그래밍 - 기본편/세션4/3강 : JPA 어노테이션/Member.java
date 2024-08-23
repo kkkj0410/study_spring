08d3eaa7b91c9af277d8d3cf2120383c936bcf0a
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-24 08:57:05 +0900

    어노테이션 종류
    ## 어노테이션 종류
    
    - @Id
        - 테이블에서 고유한 값을 가지는 변수
    - @Column(name = “asd”)
        - 해당 변수는 name에 있는 이름으로 테이블 변수와 매칭됨
    - @Enumerated(EnumType.STRING)
        - DB에서 enum타입을 쓸 수 있게 해줌
    - @Temporal(TemporalType.TIMESTAMP)
        - 날짜, 시간 등 지정
    - @Lob
        - DB에서 최대한 큰 자료형을 쓰게 해줌
    - @Transient
        - DB와 연동하지 않는 변수

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션4/3강 : JPA 어노테이션/Member.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.Date;
+
+//JPA에서 사용하는 클래스임을 명시
+@Entity
+public class Member {
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    
+    @Lob
+    private String description;
+    //Getter, Setter…
+
+    public Member(){
+
+    }
+}
