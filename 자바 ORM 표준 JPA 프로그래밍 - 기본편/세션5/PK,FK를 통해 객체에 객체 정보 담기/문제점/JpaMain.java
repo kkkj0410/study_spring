0beaa715e35c681f0f67042703a38f028537ee4e
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-27 16:16:40 +0900

    기존 코드의 문제점
    ### 기존 코드의 문제점
    
    - DB를 통해서 연관관계를 검색하므로 코드가 복잡해짐

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션5/PK,FK를 통해 객체에 객체 정보 담기/문제점/JpaMain.java
@@ -0,0 +1,39 @@
+import static hellojpa.RoleType.USER;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeamId(team.getId());
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+            
+            Long findTeamId = findMember.getTeamId();
+            Team findTeam = em.find(Team.class, findTeamId);
+            
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+}
