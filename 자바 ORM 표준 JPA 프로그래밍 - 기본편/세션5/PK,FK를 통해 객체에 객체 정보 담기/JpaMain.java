09eee83160ca57d4f840c60013a96e826103cef8
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-27 16:14:52 +0900

    PK, FK 테이블에서 데이터 져오기
    - orders→ customer_id = customers→ customer_id
    - orders→customer_id의 값은  customer테이블에서 가져오고 있음

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션5/PK,FK를 통해 객체에 객체 정보 담기/JpaMain.java
@@ -0,0 +1,51 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import static hellojpa.RoleType.USER;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+
+            //QnA)member.getTeam()하면 team 객체가 바로 꺼내지지 않나?
+            //----------member 객체가 없다고 가정하고 시작(DB에서 꺼내려고 시도할 때, 더 간편해졌음을 보여주기 위한 것)
+            Member findMember = em.find(Member.class, member.getId());
+
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
+
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+}
