7204b7b8e798a43a70ec91ef94298c6ddbd81b18
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-28 18:21:35 +0900

    Create Team.java

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션5/3강 : 양방향 연관 관계에서 주의점/2. 무한 루프/Team.java
@@ -0,0 +1,49 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+    private String name;
+
+    //Member 객체에 있는 team 변수와 매칭됨
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", members=" + members +
+                '}';
+    }
+}
