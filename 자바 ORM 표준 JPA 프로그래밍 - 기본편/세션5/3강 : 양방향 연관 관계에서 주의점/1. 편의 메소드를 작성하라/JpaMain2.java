dba5cd2caa23b3f4ebce045822c29f0ef0171865
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-28 18:19:39 +0900

    진짜 매핑과 가짜 매핑 모두 값을 변경

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션5/3강 : 양방향 연관 관계에서 주의점/1. 편의 메소드를 작성하라/JpaMain2.java
@@ -0,0 +1,48 @@
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+
+						//가짜 매핑도 값을 변경
+            team.getMembers().add(member);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+						
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+
+            for(Member m : members){
+                System.out.println("m = " + m.getUsername());
+            }
+
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+}
