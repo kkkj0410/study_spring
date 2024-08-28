88a9108bc25e725bbfc80a4d32b94976cef86be4
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-28 18:20:18 +0900

    가짜 매핑에 값을 반영하는 팁
    가짜 매핑에게도 값을 반영하는 좋은 방법
    
    →해당 클래스에 변경 사항을 적용한다

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션5/3강 : 양방향 연관 관계에서 주의점/1. 편의 메소드를 작성하라/Member.java
@@ -0,0 +1,44 @@
+
+
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public void setTeam(Team team) {
+        this.team = team;
+				
+				//진짜 매핑에 team 객체 수정 시, team 객체에게도 변경 사항을 적용
+        team.getMembers().add(this);
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
