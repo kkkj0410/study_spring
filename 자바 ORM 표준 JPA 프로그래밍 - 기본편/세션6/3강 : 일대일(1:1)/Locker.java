c8aec9a20370243c4dec31768122224518cbcc60
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-08-30 08:24:21 +0900

    Create Locker.java

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션6/3강 : 일대일(1:1)/Locker.java
@@ -0,0 +1,12 @@
+@Entity
+public class Locker {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+}
