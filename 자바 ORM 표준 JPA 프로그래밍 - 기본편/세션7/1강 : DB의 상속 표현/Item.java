db2bac0d2f161368364dd761847bd21d78bd07ad
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-09-01 08:48:21 +0900

    Create Item.java

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션7/1강 : DB의 상속 표현/Item.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+
+import javax.persistence.*;
+
+@Entity
+//위 사진과 같은 테이블 형태를 만들겠다는 어노테이션임
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
