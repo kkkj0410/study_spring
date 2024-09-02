fbc35389e339f702b8d9be3ecb3e9e60cef6bd03
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-09-03 08:23:10 +0900

    Create OrderItem.java

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션7/3강 : 예제/OrderItem.java
@@ -0,0 +1,70 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+//    @Column(name = "ORDER_ID")
+//    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+//    @Column(name = "ITEM_ID")
+//    private Long itemId;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    private int orderPrice;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
