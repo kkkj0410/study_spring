4defe5e9434b3c15b01f45a47765c6acaf5fbedf
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-09-02 09:35:21 +0900

    테스트

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션7/2강 : @MappedSuperclass/JpaMain.java
@@ -0,0 +1,45 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static hellojpa.RoleType.USER;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+}
