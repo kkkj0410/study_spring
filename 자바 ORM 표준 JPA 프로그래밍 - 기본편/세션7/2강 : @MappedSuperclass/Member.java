9b1bb752c3d8f661b696d3438ea3e3fb09b2edae
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-09-02 09:35:06 +0900

    BaseEntity 상속

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션7/2강 : @MappedSuperclass/Member.java
@@ -0,0 +1,47 @@
+
+@Entity
+public class Member extends BaseEntity{
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
+
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
