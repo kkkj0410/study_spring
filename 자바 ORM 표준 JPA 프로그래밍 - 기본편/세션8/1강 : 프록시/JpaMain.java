422a52c42cea81d832f65d4ecf7d96ec7d56ca42
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-09-04 08:31:34 +0900

    em.getReference()

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션8/1강 : 프록시/JpaMain.java
@@ -0,0 +1,38 @@
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
+
+            em.flush();
+            em.clear();
+            
+            Member m1 = em.getReference(Member.class, member1.getId());
+
+            System.out.println(m1.getClass());
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+
+
+
+}
