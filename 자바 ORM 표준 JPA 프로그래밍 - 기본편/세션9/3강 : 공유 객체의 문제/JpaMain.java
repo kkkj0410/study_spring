815aa9de7edf2aa967691ae72fabeaa0fb2520cc
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-09-10 08:11:09 +0900

    문제점

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션9/3강 : 공유 객체의 문제/JpaMain.java
@@ -0,0 +1,41 @@
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Address address = (new Address("city","street","1000"));
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
+            em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+            //member, member2 모두 newCity로 값이 바뀐다.
+            member.getHomeAddress().setCity("newCity");
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally{
+            em.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+
+}
