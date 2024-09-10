ccafa98f7016d1a4df6a725beecc21c8398ad345
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-09-11 08:13:57 +0900

    Create Member.java

---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/세션9/5강 : 값 타입 컬렉션/값 타입 컬렉션 문제 해결/Member.java
@@ -0,0 +1,11 @@
+@Entity
+public class Member{
+
+		//생략
+
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
+}
