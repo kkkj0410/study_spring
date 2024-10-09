1fede00a85d19b8501e3471591f7bc763e6ef32c
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-10-10 08:16:08 +0900

    세션 생성

---
+++ 스프링 MVC 2편/세션6/9강 : 서블릿 - 세션 오픈 소스/setSession.java
@@ -0,0 +1,22 @@
+@PostMapping("/login")
+public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request){
+    if(bindingResult.hasErrors()){
+        return "login/loginForm";
+    }
+    Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+    if(loginMember == null){
+        bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+        return "login/loginForm";
+    }
+
+
+    //true : 세션이 있으면 있는 세션 반환. 없으면 신규 세션
+    //true : 세션이 있으면 있는 세션 반환. 없으면 null
+    HttpSession session = request.getSession(true);
+    //해당 세션에 로그인 정보 저장
+    session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+
+    return "redirect:/";
+}
