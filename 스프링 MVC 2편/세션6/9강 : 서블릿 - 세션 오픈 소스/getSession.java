8888743ac77b7dce16e5e2c02be02fb45eac0a78
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-10-10 08:17:06 +0900

    세션에 있는 key를 통한 객체 가져오기

---
+++ 스프링 MVC 2편/세션6/9강 : 서블릿 - 세션 오픈 소스/getSession.java
@@ -0,0 +1,20 @@
+@GetMapping("/")
+public String homeLoginV3(HttpServletRequest request, Model model){
+    HttpSession session = request.getSession(false);
+
+    if(session == null)
+    {
+        return "home";
+    }
+
+    Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+    //회원 데이터 X면
+    if(loginMember == null){
+        return "home";
+    }
+
+    //회원 데이터 있으면
+    model.addAttribute("member", loginMember);
+    return "loginHome";
+}
