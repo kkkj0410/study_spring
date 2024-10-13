77a9a81b9de41e5c07e4519557dff91141732cb6
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-10-14 08:19:21 +0900

    인터셉터 추가(전체 코드)

---
+++ 스프링 MVC 2편/세션7/6강 : 스프링 인터셉터/WebConfig.java
@@ -0,0 +1,50 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                // /** : 서블릿 필터의 /*과 같음 (/을 가진 대상 전체 조회)
+                .addPathPatterns("/**")
+                //해당 url은 실행에서 제외한다.
+                .excludePathPatterns("/css/**", "/*.ico", "/*.ico", "/error");
+    }
+
+    //@Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        //체크 필터를 1번째 순위로 실행
+        filterRegistrationBean.setOrder(1);
+        // /* : /로 넘어오는 모든 URL에 대해서 필터 적용
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        // /* : /로 넘어오는 모든 URL에 대해서 필터 적용
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+}
