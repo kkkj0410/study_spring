e5661d15dff76a60e055fd98ce1f18ed72d47a51
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-10-26 08:19:42 +0900

    서블릿 - 체인(필터를 넣을 곳)

---
+++ 스프링부트 시큐리티 & JWT 강의/세션3 : JWT/7강 : 서블릿 필터/FilterConfig.java
@@ -0,0 +1,20 @@
+package com.cos.jwt.config;
+
+import com.cos.jwt.filter.MyFilter1;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<MyFilter1> filter1(){
+        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0); // 낮은 번호 필터가 먼저 실행
+        return bean;
+    }
+
+}
