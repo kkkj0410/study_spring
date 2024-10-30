5428885b3fe0b78788a306d23a90715a6b7d4c27
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-10-31 08:16:19 +0900

    Controller - V2 : 도메인 객체 파라미터X

---
+++ 실전! 스프링 부트와 JPA 활용2 - 웹 애플리케이션 개발/세션1/1강 : 도메인 객체는 파라미터에 있으면 안된다./MemberApiController2.java
@@ -0,0 +1,24 @@
+//Controller + ResponseBody
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+
+    @PostMapping("api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String name;
+    }
+
+}
