268549e883b226af91f0a3372965903ec02eb726
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-10-31 08:15:41 +0900

    Controller - 도메인 객체를 파라미터로 사용

---
+++ 실전! 스프링 부트와 JPA 활용2 - 웹 애플리케이션 개발/세션1/1강 : 도메인 객체는 파라미터에 있으면 안된다./MemberApiController.java
@@ -0,0 +1,23 @@
+//Controller + ResponseBody
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
