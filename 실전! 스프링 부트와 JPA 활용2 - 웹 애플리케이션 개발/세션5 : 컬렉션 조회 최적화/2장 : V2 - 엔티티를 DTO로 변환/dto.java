59efc05eb57a4264767ab91e93aef6fd3ab4156f
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-11-08 08:18:04 +0900

    콜렉션 조회 - DTO 변환

---
+++ 실전! 스프링 부트와 JPA 활용2 - 웹 애플리케이션 개발/세션5 : 컬렉션 조회 최적화/2장 : V2 - 엔티티를 DTO로 변환/dto.java
@@ -0,0 +1,41 @@
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Getter
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(OrderItemDto::new)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName; //상품명
+        private int orderPrice; //주문 가격
+        private int count; // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem){
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
