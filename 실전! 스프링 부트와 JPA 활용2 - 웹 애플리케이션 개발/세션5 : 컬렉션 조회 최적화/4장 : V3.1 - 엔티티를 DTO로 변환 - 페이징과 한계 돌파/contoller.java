4ada72c7761734d577bb10c87e838f1ce90e8d94
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-11-10 08:00:13 +0900

    Create contoller.java

---
+++ 실전! 스프링 부트와 JPA 활용2 - 웹 애플리케이션 개발/세션5 : 컬렉션 조회 최적화/4장 : V3.1 - 엔티티를 DTO로 변환 - 페이징과 한계 돌파/contoller.java
@@ -0,0 +1,31 @@
+@GetMapping("/api/v3.1/orders")
+public List<OrderDto> ordersV3_page(
+        @RequestParam(value = "offset", defaultValue = "0") int offset,
+        @RequestParam(value = "limit", defaultValue = "100") int limit)
+{
+    List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+    return orders.stream()
+            .map(OrderDto::new)
+            .collect(Collectors.toList());
+}
+
+@Getter
+static class OrderDto{
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemDto> orderItems;
+
+    public OrderDto(Order order){
+        orderId = order.getId();
+        name = order.getMember().getName();
+        orderDate = order.getOrderDate();
+        orderStatus = order.getStatus();
+        address = order.getDelivery().getAddress();
+        orderItems = order.getOrderItems().stream()
+                .map(OrderItemDto::new)
+                .collect(Collectors.toList());
+    }
+}
