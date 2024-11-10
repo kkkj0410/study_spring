95e21b988f808dd3fe5c1d2c3b24998e3348fa0c
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-11-11 08:33:47 +0900

    OrderQueryDto

---
+++ 실전! 스프링 부트와 JPA 활용2 - 웹 애플리케이션 개발/세션5 : 컬렉션 조회 최적화/5장 : V4 - JPA에서 DTO 직접 조회/OrderQueryDto.java
@@ -0,0 +1,19 @@
+@Data
+public class OrderQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
+}
