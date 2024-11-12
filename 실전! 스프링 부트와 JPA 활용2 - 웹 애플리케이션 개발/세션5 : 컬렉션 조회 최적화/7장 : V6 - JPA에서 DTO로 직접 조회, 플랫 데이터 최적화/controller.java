ae349da12a6d039502bdc817756dfc59abc1b4f1
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-11-13 08:19:16 +0900

    Create controller.java

---
+++ 실전! 스프링 부트와 JPA 활용2 - 웹 애플리케이션 개발/세션5 : 컬렉션 조회 최적화/7장 : V6 - JPA에서 DTO로 직접 조회, 플랫 데이터 최적화/controller.java
@@ -0,0 +1,14 @@
+@GetMapping("/api/v6/orders")
+public List<OrderQueryDto> ordersV6() {
+    List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+    return flats.stream()
+            .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+                            o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                    mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                            o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+            )).entrySet().stream()
+            .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                    e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                    e.getKey().getAddress(), e.getValue()))
+            .collect(toList());
+}
