f259da0dbfc8c1501498229c8e6e343466159a32
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-11-17 11:58:08 +0900

    Test

---
+++ 실전! 스프링 데이터 JPA/세션4/3강 : 공통 인터페이스 적용/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        List<Member> all2 = memberRepository.findAll();
+        assertThat(all2.size()).isEqualTo(0);
+
+    }
+
+}
