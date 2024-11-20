2209bc9991c2d8c5721de89aef8d58727c69d707
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-11-21 08:29:10 +0900

    스프링 데이터 JPA

---
+++ 실전! 스프링 데이터 JPA/세션5/3강 : @Query, 리포지토리 메소드에 쿼리 정의하기/MemberRepository.java
@@ -0,0 +1,4 @@
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+}
