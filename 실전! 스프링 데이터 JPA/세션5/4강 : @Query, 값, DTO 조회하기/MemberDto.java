c9dbcb0367389103f8f6876a0198cc781cc19180
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-11-22 08:21:48 +0900

    DTO

---
+++ 실전! 스프링 데이터 JPA/세션5/4강 : @Query, 값, DTO 조회하기/MemberDto.java
@@ -0,0 +1,13 @@
+@Data
+public class MemberDto {
+
+    private Long id;
+    private String username;
+    private String teamName;
+
+    public MemberDto(Long id, String username, String teamName) {
+        this.id = id;
+        this.username = username;
+        this.teamName = teamName;
+    }
+}
