7dc7e41073f309839957d061d51e1be3bdc679a7
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-12-10 08:26:14 +0900

    2. 페이징 자료구조 형태로 데이터 받기

---
+++ 실전! Querydsl/세션4 : 기본 문법/6강 : 페이징/paging2.java
@@ -0,0 +1,16 @@
+import static study.querydsl.entity.QMember.member;
+
+@Test
+public void paging2(){
+    QueryResults<Member> queryResults = queryFactory
+            .selectFrom(member)
+            .orderBy(member.username.desc())
+            .offset(1)
+            .limit(2)
+            .fetchResults();
+
+    assertThat(queryResults.getTotal()).isEqualTo(4);
+    assertThat(queryResults.getLimit()).isEqualTo(2);
+    assertThat(queryResults.getOffset()).isEqualTo(1);
+    assertThat(queryResults.getResults().size()).isEqualTo(2);
+}
