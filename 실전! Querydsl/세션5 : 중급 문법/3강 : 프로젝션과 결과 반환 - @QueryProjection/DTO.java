2020fe8e8dc555983b99b64821728728ad9e6ac6
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-12-20 08:47:53 +0900

    DTO

---
+++ 실전! Querydsl/세션5 : 중급 문법/3강 : 프로젝션과 결과 반환 - @QueryProjection/DTO.java
@@ -0,0 +1,15 @@
+@Data
+public class MemberDto {
+
+    private String username;
+    private int age;
+
+    public MemberDto() {
+    }
+
+    @QueryProjection
+    public MemberDto(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
