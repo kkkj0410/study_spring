7d99cd4528e4c137f6beb1a2670e32fe7a24c90a
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-12-25 12:20:40 +0900

    2. Querydsl로 Repository 생성

---
+++ 실전! Querydsl/세션6 : 실무 활용 - 순수 JPA와 Querydsl/1강 : 순수 JPA 리포지토리와 Querydsl/MemberJpaRepository.java
@@ -0,0 +1,47 @@
+@Repository
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public MemberJpaRepository(EntityManager em){
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Optional<Member> findById(Long id){
+        Member findMember = em.find(Member.class, id);
+        return Optional.ofNullable(findMember);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findAll_Querydsl(){
+        return queryFactory
+                .selectFrom(member)
+                .fetch();
+    }
+
+    public List<Member> findByUsername(String username){
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername_Querydsl(String username){
+        return queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
+    }
+
+
+}
