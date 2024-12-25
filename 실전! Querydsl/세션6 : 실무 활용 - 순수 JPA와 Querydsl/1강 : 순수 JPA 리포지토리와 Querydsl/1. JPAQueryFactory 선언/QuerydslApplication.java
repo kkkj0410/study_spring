d6bb9547225294a143080656bca0fbf3204d68cf
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-12-25 12:19:16 +0900

    2. Bean 등록

---
+++ 실전! Querydsl/세션6 : 실무 활용 - 순수 JPA와 Querydsl/1강 : 순수 JPA 리포지토리와 Querydsl/1. JPAQueryFactory 선언/QuerydslApplication.java
@@ -0,0 +1,13 @@
+@SpringBootApplication
+public class QuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
+
+}
