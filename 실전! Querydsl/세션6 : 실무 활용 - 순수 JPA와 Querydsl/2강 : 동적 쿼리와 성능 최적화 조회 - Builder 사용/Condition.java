8453b3ed9fdb9f80f19194d62f2beaf871946810
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-12-26 12:11:17 +0900

    1. Condition

---
+++ 실전! Querydsl/세션6 : 실무 활용 - 순수 JPA와 Querydsl/2강 : 동적 쿼리와 성능 최적화 조회 - Builder 사용/Condition.java
@@ -0,0 +1,9 @@
+@Data
+public class MemberSearchCondition {
+    //회원명, 팀명, 나이
+
+    private String username;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+}
