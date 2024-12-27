5d006c1230c82f28db5c22c6a2cb148fa77c38a5
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-12-27 11:25:36 +0900

    where절을 파라미터를 통해 필터링 적용하기

---
+++ 실전! Querydsl/세션6 : 실무 활용 - 순수 JPA와 Querydsl/3강 : 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용/Repository.java
@@ -0,0 +1,36 @@
+public List<MemberTeamDto> search(MemberSearchCondition condition){
+    return queryFactory
+            .select(new QMemberTeamDto(
+                    member.id.as("memberId"),
+                    member.username,
+                    member.age,
+                    team.id.as("teamId"),
+                    team.name.as("teamName")))
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(
+                    usernameEq(condition.getUsername()),
+                    teamNameEq(condition.getTeamName()),
+                    ageGoe(condition.getAgeGoe()),
+                    ageLoe(condition.getAgeLoe())
+            )
+            .fetch();
+}
+
+private BooleanExpression usernameEq(String username) {
+    return hasText(username) ? member.username.eq(username) : null;
+
+}
+
+private BooleanExpression teamNameEq(String teamName) {
+    return hasText(teamName) ? team.name.eq(teamName) : null;
+}
+
+private BooleanExpression ageGoe(Integer ageGoe) {
+    return ageGoe != null ? member.age.goe(ageGoe) : null;
+}
+
+private BooleanExpression ageLoe(Integer ageLoe) {
+    return ageLoe != null ? member.age.loe(ageLoe) : null;
+
+}
