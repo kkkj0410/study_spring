3eeaf4695b1c7be7aa7924819b63dfc37fb1c198
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2024-12-31 09:32:43 +0900

    Create searchPageComplex.java

---
+++ 실전! Querydsl/세션7 : 실무 활용 - 스프링 데이터 JPA + Querydsl/3장 : 스프링 데이터 페이징 활용1 - Querydsl 페이징 연동/searchPageComplex.java
@@ -0,0 +1,36 @@
+@Override
+public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+    List<MemberTeamDto> content = queryFactory
+            .select(new QMemberTeamDto(
+                    member.id.as("memberId"),
+                    member.username,
+                    member.age,
+                    team.id.as("teamId"),
+                    team.name.as("teamName")))
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(
+                    usernameEq(condition.getUsername()),
+                    teamNameEq(condition.getTeamName()),
+                    ageGoe(condition.getAgeGoe()),
+                    ageLoe(condition.getAgeLoe())
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+    long total = queryFactory
+            .select(member)
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(
+                    usernameEq(condition.getUsername()),
+                    teamNameEq(condition.getTeamName()),
+                    ageGoe(condition.getAgeGoe()),
+                    ageLoe(condition.getAgeLoe())
+            )
+            .fetchCount();
+
+
+    return new PageImpl<>(content, pageable, total);
+}
