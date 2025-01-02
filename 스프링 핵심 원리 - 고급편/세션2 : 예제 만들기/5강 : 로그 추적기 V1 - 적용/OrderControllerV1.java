38ba78dcf3ad6f7ba1f45fe9dd203202ebb63138
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-02 10:16:28 +0900

    Controller(로그 추적기 삽입)

---
+++ 스프링 핵심 원리 - 고급편/세션2 : 예제 만들기/5강 : 로그 추적기 V1 - 적용/OrderControllerV1.java
@@ -0,0 +1,21 @@
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
