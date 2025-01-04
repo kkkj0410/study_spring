b9b98d18619b72e821cd2a2ade2f2d8c94523377
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-04 09:27:46 +0900

    Create OrderControllerV2.java

---
+++ 스프링 핵심 원리 - 고급편/세션2 : 예제 만들기/7강 : 로그 추적기 V2 - 적용/OrderControllerV2.java
@@ -0,0 +1,21 @@
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
