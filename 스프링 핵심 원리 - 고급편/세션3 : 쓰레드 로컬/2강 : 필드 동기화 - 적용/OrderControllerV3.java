762e6c4a58b06a2be02fd6ed8d40ab87a9fbd0dc
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-06 09:20:36 +0900

    Controller

---
+++ 스프링 핵심 원리 - 고급편/세션3 : 쓰레드 로컬/2강 : 필드 동기화 - 적용/OrderControllerV3.java
@@ -0,0 +1,21 @@
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
