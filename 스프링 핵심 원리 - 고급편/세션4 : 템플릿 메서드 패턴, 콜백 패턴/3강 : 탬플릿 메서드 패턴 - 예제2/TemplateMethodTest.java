80e57f1163559dbbac83b4808cdd7bb80fdafcbe
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-10 09:37:44 +0900

    Test

---
+++ 스프링 핵심 원리 - 고급편/세션4 : 템플릿 메서드 패턴, 콜백 패턴/3강 : 탬플릿 메서드 패턴 - 예제2/TemplateMethodTest.java
@@ -0,0 +1,16 @@
+
+@Slf4j
+public class TemplateMethodTest {
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1(){
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+}
