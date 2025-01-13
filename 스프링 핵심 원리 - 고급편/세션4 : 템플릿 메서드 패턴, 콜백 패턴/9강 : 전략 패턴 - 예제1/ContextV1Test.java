82f11b7ca62a9cdee72dd2fa3e3bacbf1caf461e
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-13 10:59:46 +0900

    Test

---
+++ 스프링 핵심 원리 - 고급편/세션4 : 템플릿 메서드 패턴, 콜백 패턴/9강 : 전략 패턴 - 예제1/ContextV1Test.java
@@ -0,0 +1,17 @@
+@Slf4j
+public class ContextV1Test {
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1(){
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+}
