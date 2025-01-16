82fbeacddc5e09c6fab717e3aafa11a887e0039a
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-16 10:23:05 +0900

    탬플릿

---
+++ 스프링 핵심 원리 - 고급편/세션4 : 템플릿 메서드 패턴, 콜백 패턴/13강 : 탬플릿 콜백 패턴 - 제작/TimeLogTemplate.java
@@ -0,0 +1,17 @@
+
+@Slf4j
+public class TimeLogTemplate {
+
+    public void execute(Callback callback){
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+
+        callback.call();
+
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+}
