fa7830043257b6c3b5da10e114a26075f0c9be2c
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-17 09:59:28 +0900

    Service

---
+++ 스프링 핵심 원리 - 고급편/세션4 : 템플릿 메서드 패턴, 콜백 패턴/14강 : 템플릿 콜백 패턴 - 적용/OrderServiceV5.java
@@ -0,0 +1,18 @@
+@Service
+public class OrderServiceV5 {
+
+    private final OrderRepositoryV5 orderRepository;
+    private final TraceTemplate template;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace logTrace) {
+        this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(logTrace);
+    }
+
+    public void orderItem(String itemId) {
+        template.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
+    }
+}
