a631059f090c0f914b3dc2370cd80516a16a897d
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-21 09:54:54 +0900

    Service 프록시

---
+++ 스프링 핵심 원리 - 고급편/세션5 : 프록시 패턴, 데코레이터 패턴/11강 : 인터페이스 기반 프록시/OrderServiceInterfaceProxy.java
@@ -0,0 +1,20 @@
+@RequiredArgsConstructor
+public class OrderServiceInterfaceProxy implements OrderServiceV1 {
+
+    private final OrderServiceV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try{
+            status = logTrace.begin("OrderService.orderItem()");
+            //target 호출
+            target.orderItem(itemId);
+            logTrace.end(status);
+        }catch(Exception e){
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
