a099e1a16032fb491679baa7f74977eb23c7b6d1
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-22 10:16:54 +0900

    Client

---
+++ 스프링 핵심 원리 - 고급편/세션5 : 프록시 패턴, 데코레이터 패턴/13강 : 구체 클래스 기반 프록시 - 예시/ConcreteClient.java
@@ -0,0 +1,12 @@
+public class ConcreteClient {
+
+    private ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void execute(){
+        concreteLogic.operation();
+    }
+}
