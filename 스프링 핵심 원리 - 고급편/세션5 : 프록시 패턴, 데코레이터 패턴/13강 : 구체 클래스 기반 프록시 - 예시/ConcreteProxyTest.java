3394a80ec4ab3191a460a1adfad83596838c6914
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-22 10:17:36 +0900

    Test

---
+++ 스프링 핵심 원리 - 고급편/세션5 : 프록시 패턴, 데코레이터 패턴/13강 : 구체 클래스 기반 프록시 - 예시/ConcreteProxyTest.java
@@ -0,0 +1,11 @@
+public class ConcreteProxyTest {
+
+    @Test
+    void addProxy(){
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+
+        client.execute();
+    }
+}
