0719245c810290df3de62ff634d4f12989ae027a
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-23 09:54:33 +0900

    Controller

---
+++ 스프링 핵심 원리 - 고급편/세션5 : 프록시 패턴, 데코레이터 패턴/14강 : 구체 클래스 기반 프록시 - 적용/OrderControllerV2.java
@@ -0,0 +1,21 @@
+@Slf4j
+@RestController
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog(){
+        return "ok";
+    }
+}
