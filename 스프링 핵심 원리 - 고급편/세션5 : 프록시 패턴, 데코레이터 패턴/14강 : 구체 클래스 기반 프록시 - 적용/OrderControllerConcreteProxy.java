b50b1dfd8b1aabee7f59b2bf72b65cdc43dc6263
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-23 09:54:50 +0900

    ControllerProxy

---
+++ 스프링 핵심 원리 - 고급편/세션5 : 프록시 패턴, 데코레이터 패턴/14강 : 구체 클래스 기반 프록시 - 적용/OrderControllerConcreteProxy.java
@@ -0,0 +1,33 @@
+public class OrderControllerConcreteProxy extends OrderControllerV2 {
+
+    private final OrderControllerV2 target;
+    private final LogTrace logTrace;
+
+    public OrderControllerConcreteProxy(OrderControllerV2 target, LogTrace logTrace) {
+        super(null);
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.orderItem()");
+            //target 호출
+            String result = target.request(itemId);
+            logTrace.end(status);
+
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+    //보안상의 이슈로 noLog 함수는 로그를 찍지 않는 상황
+    @Override
+    public String noLog() {
+        return target.noLog();
+    }
+}
