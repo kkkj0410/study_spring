dc9f5fc9e9651148346d4e2d535ff1a8fc6920fc
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-28 09:43:57 +0900

    Test

---
+++ 스프링 핵심 원리 - 고급편/세션6 : 동적 프록시 기술/7강 : CGLIB/CglibTest.java
@@ -0,0 +1,21 @@
+@Slf4j
+public class CglibTest {
+
+    @Test
+    void cglib(){
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService)enhancer.create();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+}
+
+// TimeProxy 실행
+// ConcreteService 호출
+// TimeProxy 종료 resultTime=52ms
