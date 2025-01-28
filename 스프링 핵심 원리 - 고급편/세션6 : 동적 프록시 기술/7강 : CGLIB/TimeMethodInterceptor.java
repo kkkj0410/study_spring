217c11d994979b5b9b78e99d585d6f664a25d83a
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-28 09:43:45 +0900

    timeMethodInterceptor

---
+++ 스프링 핵심 원리 - 고급편/세션6 : 동적 프록시 기술/7강 : CGLIB/TimeMethodInterceptor.java
@@ -0,0 +1,24 @@
+
+@Slf4j
+public class TimeMethodInterceptor implements MethodInterceptor {
+
+    private final Object target;
+
+    public TimeMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = methodProxy.invoke(target,args);
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}ms", resultTime);
+
+        return result;
+    }
+}
