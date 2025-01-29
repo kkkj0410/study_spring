aa60d51701d0cf4b9c2c374975fd9acce70766c6
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-29 09:07:13 +0900

    ServiceImpl(비즈니스 로직)

---
+++ 스프링 핵심 원리 - 고급편/세션7 : 스프링이 지원하는 프록시/2강 : 프록시 팩토리 - 예제 코드1/ServiceImpl.java
@@ -0,0 +1,13 @@
+@Slf4j
+public class ServiceImpl implements ServiceInterface{
+
+    @Override
+    public void save(){
+        log.info("save 호출");
+    }
+
+    @Override
+    public void find(){
+        log.info("find 호출");
+    }
+}
