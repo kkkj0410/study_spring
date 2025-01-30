3ebf18ea8a6427bc830d86748e0b8ba025c45608
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-30 09:44:21 +0900

    프록시 팩토리로 CGLIB 프록시 만들기

---
+++ 스프링 핵심 원리 - 고급편/세션7 : 스프링이 지원하는 프록시/3강 : 프록시 팩토리 - 예제 코드2/concreteProxy.java
@@ -0,0 +1,17 @@
+@Test
+@DisplayName("구체 클래스만 있으면 CGLIB 사용")
+void concreteProxy(){
+    ConcreteService target = new ConcreteService();
+    ProxyFactory proxyFactory = new ProxyFactory(target);
+    proxyFactory.addAdvice(new TimeAdvice());
+    ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+
+    log.info("targetClass={}", target.getClass());
+    log.info("proxyClass={}", proxy.getClass());
+
+    proxy.call();
+
+    assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+    assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+    assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+}
