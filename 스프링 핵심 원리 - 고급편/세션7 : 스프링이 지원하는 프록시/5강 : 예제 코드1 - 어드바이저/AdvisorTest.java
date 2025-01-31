0a075602a26af526dab6a8a6ba980ceb90a152ca
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-01-31 10:24:20 +0900

    Advisor 사용 예시

---
+++ 스프링 핵심 원리 - 고급편/세션7 : 스프링이 지원하는 프록시/5강 : 예제 코드1 - 어드바이저/AdvisorTest.java
@@ -0,0 +1,14 @@
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+}
