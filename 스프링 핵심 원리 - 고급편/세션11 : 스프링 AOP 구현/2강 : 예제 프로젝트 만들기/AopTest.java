225d3afc8383fd36f0bd308e8a443504365c1290
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-02-05 13:51:28 +0900

    Create AopTest.java

---
+++ 스프링 핵심 원리 - 고급편/세션11 : 스프링 AOP 구현/2강 : 예제 프로젝트 만들기/AopTest.java
@@ -0,0 +1,26 @@
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void appInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception(){
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
