3cbb506a781716de28b7848e9c2031ca73f235d8
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-02-07 12:32:26 +0900

    Create AspectV2.java

---
+++ 스프링 핵심 원리 - 고급편/세션11 : 스프링 AOP 구현/4강 : 스프링 AOP 구현2 - 포인트컷 분리/AspectV2.java
@@ -0,0 +1,16 @@
+
+@Aspect
+@Slf4j
+public class AspectV2 {
+
+    //hello.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    private void allOrder(){}
+
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+}
