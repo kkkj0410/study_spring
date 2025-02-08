0a880d14a60468796aeb52390851bf41913a2193
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-02-08 10:24:28 +0900

    Create AspectV3.java

---
+++ 스프링 핵심 원리 - 고급편/세션11 : 스프링 AOP 구현/5강 : 스프링 AOP 구현3 - 어드바이스 추가/AspectV3.java
@@ -0,0 +1,35 @@
+@Aspect
+@Slf4j
+public class AspectV3 {
+
+    //hello.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    private void allOrder(){}
+
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService(){}
+
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+
+    //hello.aop.order 패키지와 하위 패키지 이면서 클래스 이름 패턴이 *Service
+    // => OrderService
+    @Around("allOrder() && allService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable{
+        try{
+            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            return result;
+        }catch(Exception e){
+            log.info("[트랜잭션 롤벡] {}", joinPoint.getSignature());
+            throw e;
+        }finally{
+            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
+}
