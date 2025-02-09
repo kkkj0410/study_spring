bafaf10cca2f25601fb6994423a70683ece4c738
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-02-09 10:45:46 +0900

    Create AopTest.java

---
+++ 스프링 핵심 원리 - 고급편/세션11 : 스프링 AOP 구현/6강 : 스프링 AOP 구현4 - 포인트컷 참조/AopTest.java
@@ -0,0 +1,31 @@
+@Slf4j
+@SpringBootTest
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void appInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception(){
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
