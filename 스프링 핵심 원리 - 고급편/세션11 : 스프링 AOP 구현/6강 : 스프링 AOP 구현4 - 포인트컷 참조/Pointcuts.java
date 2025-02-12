27030147a414a55393d1f694a460e71f0ec01fb0
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-02-12 14:24:04 +0900

    Create Pointcuts.java

---
+++ 스프링 핵심 원리 - 고급편/세션11 : 스프링 AOP 구현/6강 : 스프링 AOP 구현4 - 포인트컷 참조/Pointcuts.java
@@ -0,0 +1,13 @@
+public class Pointcuts {
+
+    //hello.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    public void allOrder(){}
+
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService(){}
+
+    // allOrder && allService
+    @Pointcut("allOrder() && allService()")
+    public void orderAndService() {}
+}
