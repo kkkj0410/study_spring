31381977f2a61dcc6daa4ea6ae34c20a8d15e9d9
kkkj0410 <156406222+kkkj0410@users.noreply.github.com>
2025-02-13 15:51:28 +0900

    Create AspectV5Order.java

---
+++ 스프링 핵심 원리 - 고급편/세션11 : 스프링 AOP 구현/7강 : 스프링 AOP 구현5 - 어드바이스 순서/AspectV5Order.java
@@ -0,0 +1,34 @@
+@Slf4j
+public class AspectV5Order {
+
+    @Aspect
+    @Order(1)
+    public static class LogAspect{
+        @Around("hello.aop.order.aop.Pointcuts.allOrder()")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[log] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
+    @Aspect
+    @Order(2)
+    public static class TxAspect{
+        @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+        public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable
+        {
+            try {
+                log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+                Object result = joinPoint.proceed();
+                log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+                return result;
+            } catch (Exception e) {
+                log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+                throw e;
+            } finally {
+                log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+            }
+        }
+    }
+
+}
